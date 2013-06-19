314d649b6a547d44b327bb71748750edbefadcef
kzeleny <kzeleny@gmail.com>
2013-06-19 17:04:04 -0600

    Update screensaver.py
    Added fullscreen options

--- screensaver.py
+++ screensaver.py
@@ -1,6 +1,22 @@
-import xbmc
-#shell for screensaver actual work takes place in service which monitors screensaverActivated
+#dummy screensaver will set screen to black and go fullscreen if windowed
 
+import xbmc
+import xbmcaddon
+
+addon = xbmcaddon.Addon()
+do_fullscreen = addon.getSetting('do_fullscreen')
+exit_requested = False
+class MyMonitor(xbmc.Monitor): 
+	def __init__( self):
+		pass
+		
+	def onScreensaverDeactivated(self):
+		global exit_requested
+		exit_requested = True
+		
 if __name__ == '__main__':
-	while (not xbmc.abortRequested):
+	if do_fullscreen == 'true':
+		if not xbmc.getCondVisibility("System.IsFullscreen"):
+			xbmc.executebuiltin('xbmc.Action(togglefullscreen)')
+	while (not xbmc.abortRequested) and (not exit_requested): 
 		xbmc.sleep(1000)
